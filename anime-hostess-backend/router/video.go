7ef8d92668eb759238e4032a419642213ce8d498
juzeon <812312770@qq.com>
2022-01-17 23:04:34 +0800

    docs：添加示例配置文件

--- anime-hostess-backend/router/video.go
+++ anime-hostess-backend/router/video.go
@@ -5,12 +5,11 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/juzeon/anime-hostess/include"
 	"github.com/juzeon/anime-hostess/service"
-	"net/http"
 )
 
 func RegisterVideoRouters(video *gin.RouterGroup) {
 	video.GET("/list", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, service.VideoList())
+		ctx.JSON(200, service.VideoList())
 	})
 	video.GET("/stream/:hash", include.ValidateFields(func(valid *validation.Validation, ctx *gin.Context) {
 		valid.Required(ctx.Param("hash"), "hash")
